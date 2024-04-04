056167eda486e037748989103dacc9e1fabd27cb
Your Name <you@example.com>
2024-04-04 16:20:02 +0900

    연습

---
+++ mybtisTest/src/main/java/kr/com/ezen/Mainclass.java
@@ -0,0 +1,55 @@
+package kr.com.ezen;
+
+import java.io.InputStream;
+import java.util.List;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+public class Mainclass {
+
+	public static void main(String[] args) {
+
+
+		
+		
+		try {
+		String resource = "kr/com/ezen/mybatis-config.xml";
+		InputStream inputStream = Resources.getResourceAsStream(resource);
+		SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+		
+		//System.out.println(sqlSessionFactory);
+
+		SqlSession session = sqlSessionFactory.openSession(true);
+
+		MapperInterface mapper = session.getMapper(MapperInterface.class);
+		
+		System.out.println("session :"+session);
+	
+		MemberVO vo = new MemberVO();
+		
+		vo.setId(1);
+		vo.setName("마초");
+		vo.setPhone("010-3333-9999");
+		vo.setAddress("서울시 도봉구");
+		
+//		mapper.deleteMember(1);
+//		mapper.updateMember(vo);
+		
+//		int result = mapper.insertMember(vo);
+//		System.out.println("성공이면"+result);
+		
+		//vo = mapper.selectMemberOne(2);
+		List<MemberVO> list = mapper.selectMemberList();
+		
+		
+		for(MemberVO v2 : list)
+			System.out.println(v2);
+		
+		}catch(Exception e) {
+		e.printStackTrace();
+	}
+
+}
+}
